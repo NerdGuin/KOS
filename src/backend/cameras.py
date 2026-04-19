64edba60c631ae33fa81ce6856ebd4e8ebf00ab5
NerdGuin <victor133br@gmail.com>
2026-04-19 16:58:21 -0300

    abc

--- src/backend/cameras.py
+++ src/backend/cameras.py
@@ -1,24 +1,28 @@
 from fastapi.responses import StreamingResponse
 import cv2
 
-def gen_camera(index=0):
+def gen_camera(index="/dev/video0"):
     cap = cv2.VideoCapture(index)
 
     if not cap.isOpened():
-        return {"error": "Não foi possível acessar a câmera" }
+        raise RuntimeError("Não foi possível acessar a câmera")
 
-    while True:
-        success, frame = cap.read()
-        if not success:
-            continue
+    try:
+        while True:
+            success, frame = cap.read()
+            if not success:
+                continue
 
-        _, buffer = cv2.imencode('.jpg', frame)
-        frame_bytes = buffer.tobytes()
+            _, buffer = cv2.imencode('.jpg', frame)
+            frame_bytes = buffer.tobytes()
 
-        yield (
-            b'--frame\r\n'
-            b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n'
-        )
+            yield (
+                b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n'
+            )
+    finally:
+        print("Liberando câmera")
+        cap.release()
 
 def camera_stream(cam_id: int):
     return StreamingResponse(
